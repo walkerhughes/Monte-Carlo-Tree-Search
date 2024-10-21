7adf48383b333ad138430a34ba1d4213cf2428a3
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-10-21 09:00:29 -0700

    initial commit

---
+++ mcts/settings/constants.py
@@ -0,0 +1,7 @@
+import os
+
+LLAMA_MODEL = "llama3-8b-8192"
+OPENAI_MODEL = "gpt-4o-mini"
+GROQ_ENDPOINT = "https://api.groq.com/openai"
+OPENAI_ENDPOINT = "https://api.openai.com"
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
